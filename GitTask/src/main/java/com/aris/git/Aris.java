6652af3537328c519fd03b347e3ffe1f1e2f3e7f
Vinayaka K <Vinayaka K@LAPTOP-EHELS7V2>
2022-01-09 17:25:49 +0530

    dd

--- GitTask/src/main/java/com/aris/git/Aris.java
+++ GitTask/src/main/java/com/aris/git/Aris.java
@@ -3,7 +3,7 @@ package com.aris.git;
 public class Aris {
 
 	public static void main(String[] args) {
-		String s = "  abc  def\t     vv";
+		String s = "  abc  def\t     vv"; 
 		s = s.strip();
 		System.out.println(s);
 	}
